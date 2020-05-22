e57028e813ac637e5ca6d505f7449c9a31a6e149
Belle Lee <letiya66@gmail.com>
2020-05-22 00:30:45 -0700

    Create a test class, StarbuzzCoffee

---
+++ src/chapter3/StarbuzzCoffee.java
@@ -0,0 +1,20 @@
+package chapter3;
+
+public class StarbuzzCoffee {
+	
+	public static void main(String[] args) {
+		Beverage beverage = new Espresso();
+		System.out.println(beverage.getDescription() + " $" + beverage.cost());
+		
+		Beverage beverage2 = new HouseBlend();
+		// Make a double mocha.
+		beverage2 = new Mocha(beverage2);
+		beverage2 = new Mocha(beverage2);
+		// Add a whip
+		beverage2 = new Whip(beverage2);
+		
+		System.out.println(beverage2.getDescription() + " $" + beverage2.cost());
+		
+	}
+
+}
