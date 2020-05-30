b0a958208125ad64b628db6eaa95a4337892f3a5
Belle Lee <letiya66@gmail.com>
2020-05-30 15:53:56 -0700

    Create a simple control with only one remote control - Turn on light.

---
+++ src/chapter6/Light.java
@@ -0,0 +1,12 @@
+package chapter6;
+
+public class Light {
+	
+	public void on() {
+		System.out.println("Light is turing on...");
+	}
+	
+	public void off() {
+		System.out.println("Light is turing off...");
+	}
+}
---
+++ src/chapter6/SimpleRemoteControl.java
@@ -0,0 +1,19 @@
+package chapter6;
+
+public class SimpleRemoteControl {
+	
+	Command slot;
+	
+	public SimpleRemoteControl() {
+		
+	}
+	
+	public void setCommand(Command command) {
+		slot = command;
+	}
+	
+	public void buttonWasPressed() {
+		slot.execute();
+	}
+	
+}
