42c51554514a88640b7a3053326006e0cbfc531c
Belle Lee <letiya66@gmail.com>
2020-06-02 23:44:03 -0700

    Implement an EnumerationAdapter which makes an Enumeration like an Iterator. Client => Iterator. Adaptee => Enumeration.

---
+++ src/chapter7/EnumerationIterator.java
@@ -0,0 +1,30 @@
+package chapter7;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+
+// Adapting Enumeration to Iterator. It has to look like an Iterator.
+public class EnumerationIterator implements Iterator {
+
+	Enumeration enumerator;
+	
+	public EnumerationIterator(Enumeration enumerator) {
+		this.enumerator = enumerator;
+	}
+	
+	@Override
+	public boolean hasNext() {
+		return enumerator.hasMoreElements();
+	}
+
+	@Override
+	public Object next() {
+		return enumerator.nextElement();
+	}
+	
+	public void remove() {
+		System.out.println("Iterator does not have remove()");
+		throw new UnsupportedOperationException();
+	}
+
+}
