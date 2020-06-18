480cef345c349c4be74c4ed99eed5a7c0071858d
Belle Lee <letiya66@gmail.com>
2020-06-17 21:25:18 -0700

    Implement the Menu Item.

--- src/chapter9/MenuItem.java
+++ src/chapter9/MenuItem.java
@@ -1,6 +1,6 @@
 package chapter9;
 
-public class MenuItem {
+public class MenuItem extends MenuComponent {
 	
 	private String name;
 	private String description;
@@ -28,5 +28,14 @@ public class MenuItem {
 	
 	public boolean isVegetarian() {
 		return vegetarian;
+	}
+	
+	public void print() {
+		System.out.print(" " + getName());
+		if (isVegetarian()) {
+			System.out.print("(v)");
+		}
+		System.out.println(", " + getPrice());
+		System.out.println("   -- " + getDescription());
 	}
 }
