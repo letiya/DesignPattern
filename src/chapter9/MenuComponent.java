c7e9a79a817306648d082b6a31583db834dfa15d
Belle Lee <letiya66@gmail.com>
2020-06-17 21:19:54 -0700

    Implement the Menu Component.

---
+++ src/chapter9/MenuComponent.java
@@ -0,0 +1,42 @@
+package chapter9;
+
+// We use abstract class(instead of interface) here so that we can provide default implementations for methods.
+public abstract class MenuComponent {
+	
+	// 1. Composite(Menu) methods.
+	public void add(MenuComponent menuComponent) {
+		// Because some of these methods only make sense for MenuItems, and some only make sense for Menus, the default implementation is UnsupportedOperationException. 
+		// That way, if MenuItem or Menu doesn’t support an operation, they don’t have to do anything, they can just inherit the default implementation
+		throw new UnsupportedOperationException();
+	}
+	
+	public void remove(MenuComponent menuComponent) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public MenuComponent getChild(int i) {
+		throw new UnsupportedOperationException();
+	}
+	
+	// 2. Operation methods(Mainly by MenuItems).
+	public String getName() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public String getDescription() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public double getPrice() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public boolean isVegetarian() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void print() {
+		throw new UnsupportedOperationException();
+	}
+	
+}
