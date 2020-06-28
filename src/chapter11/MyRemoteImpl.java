d22fdec220acb9159c648d969994c650061f7b0c
Belle Lee <letiya66@gmail.com>
2020-06-28 15:12:50 -0700

    Make the Remote service - 2. Make a Remote implementation

---
+++ src/chapter11/MyRemoteImpl.java
@@ -0,0 +1,18 @@
+package chapter11;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+// In order to work as a remote service object, your object needs some functionality related to 'being remote'. The simplest way is to extend UnicastRemoteObject
+public class MyRemoteImpl extends UnicastRemoteObject implements MyRemote {
+	
+	protected MyRemoteImpl() throws RemoteException {
+		super();
+	}
+
+	@Override
+	public String sayHello() throws RemoteException {
+		return "Server says, 'Hey'";
+	}
+
+}
