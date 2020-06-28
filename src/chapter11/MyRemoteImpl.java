9df4c4059b15a6ec580e988b3802d6308e7f4061
Belle Lee <letiya66@gmail.com>
2020-06-28 15:07:09 -0700

    Revert "Make the Remote service - 3. Generate stubs and skeletons"
    This reverts commit 6aff44f22519d37f93c23fbbf207affa5c31e827.

--- src/chapter11/MyRemoteImpl.java
+++ src/chapter11/MyRemoteImpl.java
@@ -3,7 +3,7 @@ package chapter11;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-// In order to work as a remote service object, your object needs some functionality related to 'being remote'. The simplest way is to extend UnicastRemoteObject
+// In order to work as a remote service object, your object needs some functionality related to ‘being remote’. The simplest way is to extend UnicastRemoteObject
 public class MyRemoteImpl extends UnicastRemoteObject implements MyRemote {
 	
 	protected MyRemoteImpl() throws RemoteException {
