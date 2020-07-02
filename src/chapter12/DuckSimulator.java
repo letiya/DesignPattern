9217a5419697d41a71cbbf4ff4aea20a635758f7
Belle Lee <letiya66@gmail.com>
2020-07-01 18:43:28 -0700

    Add the goose to our duck simulator

--- src/chapter12/DuckSimulator.java
+++ src/chapter12/DuckSimulator.java
@@ -12,13 +12,15 @@ public class DuckSimulator {
 		Quackable redheadDuck = new RedheadDuck();
 		Quackable duckCall = new DuckCall();
 		Quackable rubberDuck = new RubberDuck();
+		Quackable gooseDuck = new GooseAdapter(new Goose());
 		
-		System.out.println("Duck Simulator");
+		System.out.println("Duck Simulator: With Goose Adapter");
 		
 		simulate(mallardDuck);
 		simulate(redheadDuck);
 		simulate(duckCall);
 		simulate(rubberDuck);
+		simulate(gooseDuck);
 	}
 	
 	private void simulate(Quackable duck) {
